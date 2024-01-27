bd73c51c8fa5fd35d781eadaf85bc1713841814c
kaykay6895 <kayodearemu76@gmail.com>
2024-01-27 18:13:50 +0000

    modified task5.js

--- module 3/task3.js
+++ module 3/task3.js
@@ -1,1 +1,8 @@
-alert('Hello world')
+ 
+const myfavlanguage = 'Javascript'
+
+const age = 20
+const name = 'kayode'
+const setence = 'my name is '
+
+console.log(sentence)
---
+++ module 3/task5.js
@@ -0,0 +1,8 @@
+ const myfavlanguage ='javaScript'
+
+ const age =20
+ const name = 'kayode'
+ const sentence = 'My name is ' + name +'. '+'I am  '+age+' years old'
+
+ console.log(sentence)
+ 
