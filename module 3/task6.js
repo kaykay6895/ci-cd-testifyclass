ed112992fe7e02b786287e42951faf17753b9c7b
kaykay6895 <kayodearemu76@gmail.com>
2024-02-03 23:14:35 +0000

    modified task6.js

--- module 3/task6.js
+++ module 3/task6.js
@@ -1,12 +1,8 @@
-//const side1 = 3
-//const side2 = 3
-//const side3 =3
+const side1 = 3
+const side2 = 3
+const side3 =3
 
-//if (side1 === side2 && side2 === side3) {
-    //console.log('equilateral triangle')
-
-
-else if (side1 === side2 || side1 === side3 || side2 === side3) {
-    cosole.log('isosceles triangle')
-
+if (side1 === side2 && side2 === side3) {
+    console.log('equilateral triangle')
 }
+
