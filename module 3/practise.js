cc10b6ef849c7462bfad98eef8ec7e5c58cac40f
kaykay6895 <kayodearemu76@gmail.com>
2024-02-03 22:59:20 +0000

    modified task6.js

---
+++ module 3/practise.js
@@ -0,0 +1,21 @@
+//const firstName = kayode;
+//const surName = aremu;
+
+//console.log('firstName + surName');
+
+//console.log('Math.round(2.2');
+
+let calculation = 2 + 2;
+console.log(calculation)
+
+console.log(calculation + 2)
+
+
+
+
+
+
+
+
+
+
--- module 3/task4.js
+++ module 3/task4.js
@@ -1,2 +1,22 @@
-alert("Hello world!")
+//alert("Hello world!")
+
+//const firstname = ('kayode')
+//const surname = ('aremu')
+//console.log(firstname  +  surname)
+
+const age = 15
+
+if(age>=18) {
+    console.log('you are eligible to vote')
+} else 
+
+{console.log('you are too young to vote')
+
+
+} 
+
+
+
+
+
 
