dfa88ca8d799a4752428598184871ed661ea8e3f
kaykay6895 <kayodearemu76@gmail.com>
2024-02-04 22:16:31 +0000

    modified practise.js

--- module 3/practise.js
+++ module 3/practise.js
@@ -5,10 +5,77 @@
 
 //console.log('Math.round(2.2');
 
-let calculation = 2 + 2;
-console.log(calculation)
+//let calculation = 2 + 2;
+//console.log(calculation)
 
-console.log(calculation + 2)
+//console.log(calculation + 2)
+
+
+//let x =10
+//let y =20
+
+//console.log(x+y)
+
+//let highincome = true;
+//let goodcreditscore = true;
+//let eligibleforloan = highincome && goodcreditscore
+
+//console.log(eligibleforloan)
+
+
+//let x = 10;
+
+//let y = (x > 5) && (x < 15); 
+
+//console.log(x&&y)
+
+
+//let x = 1; 
+
+//let y = x !== 2;
+//console.log(x&&y)
+
+//let a = 'red'
+//let b = 'green'
+
+//console.log(a&&b)
+
+// if hour is between 6am and 12pm: Good morning
+// if it is between 12pm and 6pm; Good Afternoon
+// otherwise: Good evening 
+
+let hour = 5;
+
+if (hour >=6 && hour < 12)
+console.log('Good morning');
+else if (hour >= 12 && hour <18)
+console.log('Good afternoon')
+else 
+console.log('Good evening')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
+
+
+
 
 
 
