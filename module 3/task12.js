7fbc32b98df7b9c4200be87e4356b2b4394e81f1
kaykay6895 <kayodearemu76@gmail.com>
2024-02-06 21:16:10 +0000

    modified task12.js

--- module 3/task12.js
+++ module 3/task12.js
@@ -1,10 +1,8 @@
 
 const myfriendArray = ['victor', 'bola', 'kemi', 'paul', 'tosin', 'wale', 'victoria']
 const numArray = [90 ,60 ,50]
-const emptyArray = []
-emptyarray[0] = 'firstElement'
-emptyArray[1] = 'secondElement'
-emptyArray[3] = 'forthelement'
+
+
 
 console.log(myfriendArray[2])
 
