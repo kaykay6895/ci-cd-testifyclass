d3f13dcc984ed159eee9eb645e79a3b8edbfa244
kaykay6895 <kayodearemu76@gmail.com>
2024-02-08 20:45:50 +0000

    modified task6.js

---
+++ module 3/task6.js
@@ -0,0 +1,15 @@
+const number = 16
+if (number%2===0) {
+    console.log('number is even')
+} else {
+    console.log('number is odd')
+
+}
+
+const number1 = 13 
+if (number1%2===0) {
+    console.log('number is even')
+    
+} else {
+    console.log('number is odd')
+}
