0405ff700eee78d04198c84f3341e22dcba839ca
kaykay6895 <kayodearemu76@gmail.com>
2024-02-08 21:01:04 +0000

    modified task10.js

---
+++ module 3/task10.js
@@ -0,0 +1,11 @@
+function calculateAreaOfrectangle (length, width) {
+
+const mutiply = length * width;
+return mutiply;
+
+}
+
+const moreOp = calculateAreaOfrectangle(10,8)
+
+console.log(moreOp)
+
