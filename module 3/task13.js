0d3e3277612db6560756cd3f778ee604ab0fe74a
kaykay6895 <kayodearemu76@gmail.com>
2024-02-14 20:51:19 +0000

    modified task16.13.js

---
+++ module 3/task13.js
@@ -0,0 +1,13 @@
+const myObject = {
+
+
+    book:'the men called God',
+    description:'autobiography stories',
+    numberofpage: 100,
+    author:'tyler',
+    reading:'true'
+} 
+
+console.log(' book')
+
+
